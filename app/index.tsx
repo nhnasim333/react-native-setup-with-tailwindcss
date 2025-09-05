64f06dc6c6ebbe4ea7b24b604fb6a0e5034781e1
Nasim <mdnasimhosen333@gmail.com>
2025-09-05 20:42:40 +0600

    initialize project with Expo, Tailwind CSS, and TypeScript setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-blue-500">
+        Build your React Native app with Tailwind CSS
+      </Text>
+    </View>
+  );
+}
