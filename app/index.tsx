c5d9d95fa80c6cc942fc3f357f6b069ae3daf03b
Nasim <mdnasimhosen333@gmail.com>
2025-09-05 20:43:19 +0600

    add welcome message in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-2xl font-bold text-blue-500">
-        Build your React Native app with Tailwind CSS
+        Build your React Native app with Tailwind CSS!
       </Text>
     </View>
   );
